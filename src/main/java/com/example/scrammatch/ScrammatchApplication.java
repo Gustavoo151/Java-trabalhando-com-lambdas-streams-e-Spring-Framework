b26806742bdf1f3c184fc4ef6815d3c9f6338ee2
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2024-06-29 02:52:53 -0300

    Adcionando a interface CommandLineRunner para usar a linha de comando no terminal com spring boot

--- src/main/java/com/example/scrammatch/ScrammatchApplication.java
+++ src/main/java/com/example/scrammatch/ScrammatchApplication.java
@@ -1,13 +1,18 @@
 package com.example.scrammatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScrammatchApplication {
+public class ScrammatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrammatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello, Spring Boot!");
+	}
 }
