f027009fc983014e43e30d60d0a8e665c993ad83
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2024-06-29 02:46:56 -0300

    Subindo projeto Spring

---
+++ src/test/java/com/example/scrammatch/ScrammatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scrammatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrammatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
